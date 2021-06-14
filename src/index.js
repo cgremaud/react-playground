189a46a88de3935c6ba095c587510fbd67818a0e
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-14 13:04:31 -0500

    initial commit, basic clock works

--- src/index.js
+++ src/index.js
@@ -4,9 +4,44 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.foo = setInterval( //I guess this line just declares and initializes this field every time this method is called? 
+      () => this.tick(), 
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+      clearInterval(this.timerId); //this is only called if the clock element is removed from the DOM so the count doesn't continue while it's not displayed. 
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Clock />
   </React.StrictMode>,
   document.getElementById('root')
 );
